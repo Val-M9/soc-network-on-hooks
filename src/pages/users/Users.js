aff80ef6ffc872a1d7932078972d26ceb80ce3af
Val_M 9 <v.mischenko90@gmail.com>
2021-09-11 17:02:37 +0300

    move some logic to useUser hook

--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -1,24 +1,13 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchUsers, toggleFollowing } from "../../redux/actions/users";
-import style from "./users.module.css";
 import User from "./User";
 import Paginator from "../../components/paginator/Paginator";
+import useUsers from "../../hooks/useUsers";
+import style from "./users.module.css";
 
 export default function Users() {
+  const { users, currentPage, quantityOnPage, totalCount } = useUsers();
   const dispatch = useDispatch();
-  const { users, currentPage, quantityOnPage, totalCount } = useSelector(({ usersPage }) => {
-    return {
-      users: usersPage.users,
-      currentPage: usersPage.currentPage,
-      quantityOnPage: usersPage.quantityOnPage,
-      totalCount: usersPage.totalCount,
-    };
-  });
-
-  useEffect(() => {
-    dispatch(fetchUsers(currentPage, quantityOnPage));
-  }, [dispatch, currentPage, quantityOnPage]);
 
   const onChangePage = (pageNumber) => {
     dispatch(fetchUsers(pageNumber, quantityOnPage));
